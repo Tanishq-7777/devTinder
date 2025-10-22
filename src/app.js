59c313674e014d6a333118ec92fe59634b0bb7a6
Tani2007 <saxenatanishq4567@gmail.com>
2025-10-22 18:45:45 +0530

    routings with postman

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-app.use("/test", (req, res) => {
-  res.send("Namaste");
-});
-app.use("/hello", (req, res) => {
-  res.send("Namaste on Your request.");
-});
+app.get("/user",(req,res) => {
+  res.send({firstname:"Tanishq",lastname:"Saxena"})
+})
+app.post("/user",(req,res) => {
+  res.send("user data is sent successfully.");
+})
+app.delete("/user",(req,res) => {
+  res.send("user data is deleted successfully.")
+})
+app.patch("/user",(req,res) => {
+  res.send("user data is updated successfully.")
+})
 app.listen(9999, () => {
   console.log("Server listing on port 9999.");
 });
