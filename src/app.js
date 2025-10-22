12a0377a8fd74a432b65a44b18ca942f7ef36642
Tani2007 <saxenatanishq4567@gmail.com>
2025-10-22 22:52:13 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const app = express();
-app.get("/user",(req,res) => {
-  res.send({firstname:"Tanishq",lastname:"Saxena"})
-})
-app.post("/user",(req,res) => {
-  res.send("user data is sent successfully.");
-})
-app.delete("/user",(req,res) => {
-  res.send("user data is deleted successfully.")
-})
-app.patch("/user",(req,res) => {
-  res.send("user data is updated successfully.")
-})
+
+app.get('/user/:userid/:name', (req, res) => {
+  console.log(req.params)
+  res.send("This is the get api call");
+});//write anything in place of * while calling this api.
+
 app.listen(9999, () => {
   console.log("Server listing on port 9999.");
 });
