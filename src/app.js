ed7dedcce666c40b148898e7e4bf6028fa73efc4
Tani2007 <saxenatanishq4567@gmail.com>
2025-10-23 14:29:14 +0530

    palying with routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get('/user/:userid/:name', (req, res) => {
-  console.log(req.params)
-  res.send("This is the get api call");
-});//write anything in place of * while calling this api.
+app.use("/user",(req,res,next) => {
+  //do not send anything. --> will fall browser to infinite loop.
+  console.log("Logged Item of Response 1");
+  next();
+},(req,res,next) => {
+  console.log("Logged Item of Response 2");
+  next();
+})
 
 app.listen(9999, () => {
   console.log("Server listing on port 9999.");
