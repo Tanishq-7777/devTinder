1c84ff7ae3a2886c5e91b8e3e6f9ba9a317d9c48
Tani-7777 <saxenatanishq4567@gmail.com>
2025-10-31 17:10:24 +0530

    jwt token expire time

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth");
 const app = express();
 app.use(express.json()); //to convert the json data to js object for all api.
 //post API
@@ -42,7 +43,9 @@ app.post("/login", async (req, res) => {
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (isPasswordValid) {
       //Add the token and Cookie
-      const token = jwt.sign({ _id: user._id }, "DEV@TINDER$2025");
+      const token = jwt.sign({ _id: user._id }, "DEV@TINDER$2025", {
+        expiresIn: "1d",
+      });
       res.cookie("token", token);
 
       res.send("LoggedIn Successfully...");
@@ -55,80 +58,19 @@ app.post("/login", async (req, res) => {
 });
 
 //Profile API
-app.get("/profile", async (req, res) => {
+app.get("/profile", userAuth, async (req, res) => {
   try {
-    const cookies = req.cookies;
-    const { token } = cookies;
-    if (!token) {
-      throw new Error("Token is not valid");
-    }
-    const decodedMessage = jwt.verify(token, "DEV@TINDER$2025");
-    const { _id } = decodedMessage;
-
-    const user = await User.findById({ _id });
-    if (!user) {
-      throw new Error("User is not present");
-    }
+    const user = req.user;
     res.send(user);
   } catch (err) {
     res.send(err.message);
   }
 });
 
-app.get("/user", async (req, res) => {
-  try {
-    const user = await User.find(req.body); //*To Find One Document
-    if (!user.length) {
-      res.status(404).send("invalid User");
-    } else {
-      res.send(user);
-    }
-  } catch (err) {
-    res.send("User not found");
-  }
-});
-app.get("/feed", async (req, res) => {
-  try {
-    //*To find all Document
-    const user = await User.find({});
-    if (!user.length) {
-      res.status(404).send("invalid User");
-    } else {
-      res.send(user);
-    }
-  } catch (err) {
-    res.send("User not found");
-  }
-});
-app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-  await User.findByIdAndDelete(userId);
-  res.send("Deleted");
-});
-//Update Data of the User.
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-  try {
-    const ALLOWED_UPDATES = ["photoUrl", "about", "skills"];
-    const isUpdatedAllowed = Object.keys(data).every((k) => {
-      return ALLOWED_UPDATES.includes(k);
-    });
-    const set = new Set(data.skills);
-    data.skills = Array.from(set);
-
-    if (!isUpdatedAllowed) {
-      throw new Error("This Data Can not Be Updated");
-    }
-    const user = await User.findByIdAndUpdate(userId, data, {
-      returnDocument: "after",
-      runValidators: true,
-    });
-    res.send("Updated");
-  } catch (err) {
-    res.send("Error 404");
-  }
+//SendConnectionRequest
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  const user = req.user;
+  res.send(user.firstName + " sent the connection request");
 });
 
 connectDB()
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,12 +1,23 @@
-const authUser = (req,res,next) => {
-  let accessToken = true;
-  if(!accessToken){
-    res.send("You are not an authenticated user.");
-  }
-  else{
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Token Not Valid!!!!");
+    }
+    const decodedObj = jwt.verify(token, "DEV@TINDER$2025");
+    const { _id } = decodedObj;
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not Found!!!!");
+    }
+    req.user = user;
     next();
+  } catch (err) {
+    res.send(err.message);
   }
-}
+};
 module.exports = {
-    authUser,
-}
+  userAuth,
+};
