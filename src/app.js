f15faa480d7d04af53afb4699bb2b59cd7a76285
Tani-7777 <saxenatanishq4567@gmail.com>
2025-11-07 21:37:08 +0530

    done cors

--- src/app.js
+++ src/app.js
@@ -14,9 +14,22 @@ app.use(express.json()); //to convert the json data to js object for all api.
 //post API
 const PORT = process.env.PORT || 9999;
 app.use(cookieParser()); //parsing cookies
+const allowedOrigins = [
+  "http://localhost:5173", // for local development
+  "https://your-frontend-name.vercel.app", // your live Vercel frontend URL
+];
+
 app.use(
   cors({
-    origin: "*",
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
