2ffa789f6799545a4a2518ce30f4e42a3ba07ae9
Tani-7777 <saxenatanishq4567@gmail.com>
2025-11-25 00:21:56 +0530

    feat : chat

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const { userRouter } = require("./routes/user");
-
+const http = require("http");
+const initializeSocket = require("./utils/socket");
 const app = express();
 
 // ✅ Middlewares (order matters)
@@ -15,20 +16,16 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Allow requests with no origin (like Postman, server-side)
       if (!origin) return callback(null, true);
 
-      // Allow local development
       if (origin === "http://localhost:5173") {
         return callback(null, true);
       }
 
-      // ✅ Dynamically allow all your Vercel deployments
       if (origin.endsWith(".vercel.app")) {
         return callback(null, true);
       }
 
-      // ❌ Block everything else
       console.log("❌ Blocked by CORS:", origin);
       return callback(new Error("Not allowed by CORS"));
     },
@@ -41,13 +38,14 @@ app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 
-const PORT = process.env.PORT || 9999;
-// const PORT = 9999;
-
+// const PORT = process.env.PORT || 9999;
+const PORT = 9999;
+const server = http.createServer(app);
+initializeSocket(server);
 connectDB()
   .then(() => {
     console.log("✅ Database connected");
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
   })
---
+++ src/utils/socket.js
@@ -0,0 +1,23 @@
+const socket = require("socket.io");
+const initializeSocket = (server) => {
+  const io = socket(server, {
+    cors: {
+      origin: "http://localhost:5173",
+    },
+  });
+  io.on("connection", (socket) => {
+    //Handle Events
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const roomId = [userId, targetUserId].sort().join("_");
+      console.log(firstName + " Joined Room " + roomId);
+      socket.join(roomId);
+    });
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+      const roomId = [userId, targetUserId].sort().join("_");
+      io.to(roomId).emit("messageRecieved", { firstName, text });
+      console.log(firstName + " " + text);
+    });
+    socket.on("disconnect", () => {});
+  });
+};
+module.exports = initializeSocket;
